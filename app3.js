442a1af8e15de5f482f3c57b532007f349ddb633
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-23 07:21:54 +0000

    making folder fs/promises

---
+++ app3.js
@@ -0,0 +1,13 @@
+// import fs from "fs/promises";
+// import  readline from "readline-sync"
+
+// async function createfolder(){
+//  try {
+//        let folder= readline.question("enter folder name ")
+//        await fs.mkdir(folder)
+//  } catch (error) {
+//     console.log(error);
+    
+//  }
+// }
+// createfolder()
