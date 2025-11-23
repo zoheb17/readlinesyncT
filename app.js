a0c457a8788d43b45277c2def974f362c438ecf2
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-23 07:17:16 +0000

    readline-sync

---
+++ app.js
@@ -0,0 +1,15 @@
+import  readline from "readline-sync"
+
+
+let faname=readline.question("what is your name")
+
+
+let age =readline.question("what is your age")
+
+let email=readline.questionEMail("enter your email ")
+
+
+console.log("\n=============summary=======");
+console.log(`my  good name is ${faname}`);
+console.log(`my age :${age}`);
+console.log(`my email ${email}`);
